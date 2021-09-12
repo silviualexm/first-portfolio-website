0c558b442cc32ec0ec3c2cdf282e2082d0f2ff72
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-09-12 15:22:20 +0300

    topbar basic design

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,26 @@
 import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
 export default function Topbar() {
-	return <div className="topbar"></div>;
+	return (
+		<div className="topbar">
+			<div className="wrapper">
+				<div className="left">
+					<a href="#intro" className="logo">
+						genius.
+					</a>
+					<div className="itemContainer">
+						<Person className="icon" />
+						<span>+39 376 89 32</span>
+					</div>
+					<div className="itemContainer">
+						<Mail className="icon" />
+						<span>test@yahoo.com</span>
+					</div>
+				</div>
+
+				<div className="right">this is right</div>
+			</div>
+		</div>
+	);
 }
