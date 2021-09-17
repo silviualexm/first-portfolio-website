ecb47716076945e0f9a94bb70bc662cd70273a5f
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-09-17 12:52:19 +0300

    animated description intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,18 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+	const textRef = useRef();
+
+	useEffect(() => {
+		init(textRef.current, {
+			showCursor: true,
+			backDelay: 1500,
+			backSpeed: 60,
+			strings: ["Developer", "Designer", "Content Creator"],
+		});
+	}, []);
 	return (
 		<div className="intro" id="intro">
 			<div className="left">
@@ -13,7 +25,7 @@ export default function Intro() {
 					<h2>Hi There, I'm</h2>
 					<h1>Silviu</h1>
 					<h3>
-						Freelance <span>Designer</span>
+						Freelance <span ref={textRef}></span>
 					</h3>
 				</div>
 				<a href="#portfolio">
